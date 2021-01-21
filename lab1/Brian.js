ba3eab118abc6ab8b9f06a39445bf3003424e0a4
Originalnag <59655343+Originalnag@users.noreply.github.com>
2021-01-21 12:21:53 -0500

    Finished Lab1

---
+++ lab1/Brian.js
@@ -0,0 +1,9 @@
+import Person from "./Person.js"
+
+class Brian extends Person {
+    constructor() {
+    super("Brian", "#FFFFFF")
+    }
+}
+
+export default Brian
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Brian from './Brian.js';
+
+const b = new Brian("Brian", "#FFFFFF")
+b.speak();
